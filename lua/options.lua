8854879cc20bde64c5f657d25b00e2fde0d7537c
Nikita Bondarev <bondarev.n17@yandex.ru>
2024-10-31 09:23:21 +0300

    OInitial commit

---
+++ lua/options.lua
@@ -0,0 +1,11 @@
+require "nvchad.options"
+
+-- add yours here!
+
+-- local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!
+--
+vim.api.nvim_set_option_value("tabstop", 4, {})
+vim.api.nvim_set_option_value("shiftwidth", 4, {})
+vim.api.nvim_set_option_value("expandtab", true, {})
+-- vim.api.nvim_set_option_value("clipboard", "unnamedplus", {})
